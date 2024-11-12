9b05043dc0038bba923f935d3f227a3134d5d14c
Aliac <alimehdizadeh822@gmail.com>
2024-11-12 10:23:39 +0330

    feat: sql_introduction module added

---
+++ sql_introduction/controllers/taskController.js
@@ -0,0 +1,59 @@
+const Task = require("../models/task");
+
+exports.getTasksPage = async (req, res, next) => {
+  const tasks = await Task.getAll();
+  res.render("pages/tasks", { title: "TASKS PAGE", path: "/tasks", tasks });
+};
+
+exports.getAddTaskPage = (req, res, next) => {
+  res.render("pages/add-task", {
+    title: "ADD TASK PAGE",
+    path: "/tasks/add-task-page",
+  });
+};
+
+exports.postAddTask = async (req, res, next) => {
+  const newTask = new Task(
+    req.body.title,
+    req.body.description,
+    req.body.imageUrl
+  );
+  const result = await newTask.save();
+  res.redirect("/tasks");
+};
+
+exports.getEditTaskPage = async (req, res, next) => {
+  const taskId = req.params.taskId;
+  const task = await Task.findById(taskId);
+
+  res.render("pages/edit-task", {
+    title: "EDIT TASK PAGE",
+    path: "",
+    task,
+  });
+};
+
+exports.postEditTask = async (req, res, next) => {
+  const taskId = req.params.taskId;
+  const task = await Task.findById(taskId);
+  if (task) {
+    await Task.updateTask(
+      taskId,
+      req.body.title,
+      req.body.description,
+      req.body.imageUrl
+    );
+  }
+
+  res.redirect("/tasks");
+};
+
+exports.postDeleteTask = async (req, res, next) => {
+  const taskId = req.body.taskId;
+  const task = await Task.findById(taskId);
+  if (task) {
+    await Task.deleteTask(taskId);
+  }
+
+  res.redirect("/tasks");
+};
