2abf8ddbbad0a41f49f1caeda08e6f9f9f3f42e3
Aliac <alimehdizadeh822@gmail.com>
2024-12-03 10:14:37 +0330

    feat: cookie added for login

--- sessions_and_cookies/controllers/authController.js
+++ sessions_and_cookies/controllers/authController.js
@@ -1,3 +1,12 @@
 exports.getLoginPage = (req, res, next) => {
-  res.render("pages/auth/login", { title: "LOGIN PAGE", path: "/login" });
+  // const isAuthenticated = req.headers.cookie.split("=")[1];
+  res.render("pages/auth/login", {
+    title: "LOGIN PAGE",
+    path: "/login",
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.cookie("isLoggedIn", "true", { httpOnly: true });
+  res.redirect("/");
 };
